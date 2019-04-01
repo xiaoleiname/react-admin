9d7585e5f0517d96d1912af3def2011ccd15e1d3
xiaolei <1653349993qq.com>
2019-04-01 19:33:37 +0800

    完成login静态组件

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+//应用主组件
+import React,{ Component } from 'react';
+import { Route, Switch, Redirect} from 'react-router-dom';
+
+import Login from './pages/login';
+import Admin from './pages/admin';
+
+export default class App extends Component {
+    render() {
+        return (
+            <Switch>
+                <Route path="/login" component={Login}/>
+                <Route path="/" component={Admin}/>
+                /* 为了开发login组件设计的*/
+                <Redirect to="/login"/>
+            </Switch>
+        )
+    }
+}
